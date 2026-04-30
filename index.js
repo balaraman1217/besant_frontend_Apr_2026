b4517da6c4e4c2ee5a3bdad34674997cf35281d7
Dinesh107 <dineshjr107@gmail.com>
2026-04-30 20:26:44 +0530

    4th commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log("welcome to javascript funtion() world");
+// console.log('welcome to javascript world');
+//console.log(`welcome to javascript world ${} students`);
+
+/*
+
+ds;lafj'l;sdl;asflkdsjfk
+djlfjkdaf
+
+
+kjadfhudshfdsfuo
+
+*/
+
+
+// // window.alert("Welcome to javascript world");
+
+// document.getElementById("myH1").textContent = `Hello welcome to js world`;
+// document.getElementById("myP").textContent = "I like briyani";
+
+let a = 10;
+
+let b = 90;
+
+console.log(a + b);
