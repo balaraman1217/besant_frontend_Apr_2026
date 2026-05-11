130baac720471eadcc5bd3c379793c782aaee190
Dinesh107 <dineshjr107@gmail.com>
2026-05-11 20:08:31 +0530

    6th commit

--- index.js
+++ index.js
@@ -92,12 +92,215 @@ let students = 20;
 // let username = window.prompt("What is your username name?");
 // console.log(username);
 
-let username;
+// let username;
 
-document.getElementById("mySubmit").onclick = function() {
-   username = document.getElementById("myText").value;
-   document.getElementById("myH1").textContent = `${username} welcome to javascript`;
-}
+// document.getElementById("mySubmit").onclick = function() {
+//    username = document.getElementById("myText").value;
+//    document.getElementById("myH1").textContent = `${username} welcome to javascript`;
+// }
+
+
+// let age = window.prompt("How old are you");
+// age = Number(age)
+// age += 1;
+// console.log(age, typeof age);
+
+// let x = "lemon";
+// let y = "lemon";
+// let z = "lemon";
+
+
+// let x = 12;
+// let y = 12;
+// let z = 12;
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+// const PI = 3.14;
+
+// PI = 536789;
+
+// console.log(PI);
+
+
+
+// age = 10;
+// // false
+// if(age >= 18) {
+//    console.log("You cannot vote");
+// } 
+
+
+// age = 10;
+// // false 
+// if(age >= 18) {
+//    console.log("You are senior");
+// } else {
+//    console.log("You are Junior");
+// } 
+
+
+// let mark = 90;
+
+// if(mark < 10) {
+//    console.log("Your garde is E");
+
+// } else if(mark >= 10 && mark < 30) {
+//      console.log("Your garde is C");
+// } else if(mark >= 30 && mark <70 ) {
+//    console.log("Your garde is B");
+// } else if(mark >= 70 && mark <= 100) {
+//    console.log("Your garde is A");
+// } else {
+//    console.log("Invaild mark");
+// }
+
+
+// const myCheckbox = document.getElementById("myCheckBox");
+// const visaBtn = document.getElementById("visaBtn");
+// const mastercardBtn = document.getElementById("mastercardBtn");
+// const rupyaBtn = document.getElementById("rupyaBtn");
+// const mySubmit = document.getElementById("mySubmit");
+// const subResult = document.getElementById("subResult");
+// const paymentResult = document.getElementById("paymentResult");
+
+
+// mySubmit.onclick = function() {
+//    if(myCheckbox.checked) {
+//      subResult.textContent = `You are subscribed`;
+//    } else {
+//        subResult.textContent = `You are not subscribed`;
+//    }
+
+//    if(visaBtn.checked) {
+//       paymentResult.textContent = `You are paying with visa`;
+//    }
+// }
+
+
+
+// let age = 20;
+
+// if(age >= 18) {
+//    console.log("You can vote");
+// } else {
+//    console.log("You cannot vote");
+// }
+
+// const result = age >= 18 ? "You can vote" : "You cannot vote";
+
+// console.log(result);
+
+
+// let testScore = 96;
+
+// let Garade;
+
+
+// switch(true) {
+
+//    case testScore >= 90:
+//    Garade = "A";
+//    break;
+
+//    case testScore >= 80:
+//    Garade = "B";
+//    break;
+
+//    case testScore >= 70:
+//    Garade = "C";
+//    break;
+
+//    case testScore >= 60:
+//    Garade = "D";
+//    break;  
+   
+//    default:
+//       Garade = "F";
+
+
+// }
+
+// console.log(Garade);
+
+
+
+//12
+// 0 to n -1 = 0 to 11
+//             
+// let username = "ganesh kumar ";
+
+// charAt()
+
+// const res = username.charAt(1);
+// const res = username.indexOf("a");
+// const res = username.lastIndexOf("a");
+// const res = username.length;
+// const res = username.trim();
+// const res1= username.toLowerCase();
+// const res2= username.toUpperCase();
+
+// let result = username.startsWith(" ");
+// let result = username.endsWith(" ");
+
+
+
+// 
+
+
+
+// if(result) {
+//    console.log("Your username cant start with space");
+// } else {
+//    console.log(username);
+// }
+
+
+
+// console.log(res1, res2);
+
+
+
+// dinesh@gmail.com
+
+// const fullName = "joseph krish";
+
+// let firstName = fullName.slice(0, 6);
+// let lastName = fullName.slice(7);
+
+// let firstChar = fullName.slice(0,1)
+// let lastChar = fullName.slice(-1)
+
+
+
+// let firstName = fullName.slice(0, fullName.indexOf(" "));
+// let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+
+// let firstChar = fullName.slice(0,1)
+// let lastChar = fullName.slice(-1)
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(firstChar);
+// console.log(lastChar);
+
+
+
+
+
+
+
 
 
 
