6a25be8a08d6dfa3789faf86c88555ae534dd207
Dinesh107 <dineshjr107@gmail.com>
2026-06-02 20:19:12 +0530

    17th-commit

--- index.js
+++ index.js
@@ -830,65 +830,182 @@ kjadfhudshfdsfuo
 // fish.swim();
 
 
-class Animal {
+// class Animal {
   
-    constructor(name, age) {
-       this.name = name;   
-       this.age = age; 
-    }
+//     constructor(name, age) {
+//        this.name = name;   
+//        this.age = age; 
+//     }
 
-    move(speed) {
-        console.log(`the ${this.name} moves at speed of ${speed}kmh`);
-    }
+//     move(speed) {
+//         console.log(`the ${this.name} moves at speed of ${speed}kmh`);
+//     }
 
-}
+// }
 
-class Tiger extends Animal {
-     constructor(name, age, runSpeed) {
-        super(name, age);
-       this.runSpeed = runSpeed;   
-    }
+// class Tiger extends Animal {
+//      constructor(name, age, runSpeed) {
+//         super(name, age);
+//        this.runSpeed = runSpeed;   
+//     }
 
-    run() {
-        console.log(`This ${this.name} can run`);
-        super.move(this.runSpeed);
-    }
-}
+//     run() {
+//         console.log(`This ${this.name} can run`);
+//         super.move(this.runSpeed);
+//     }
+// }
 
-class Fish extends Animal {
-    constructor(name, age, swimSpeed) {
-         super(name, age); 
-       this.swimSpeed = swimSpeed;
+// class Fish extends Animal {
+//     constructor(name, age, swimSpeed) {
+//          super(name, age); 
+//        this.swimSpeed = swimSpeed;
         
-    }
+//     }
 
-    swim() {
-        console.log(`This ${this.name} can swim`);
-        super.move(this.swimSpeed);
-    }
-}
+//     swim() {
+//         console.log(`This ${this.name} can swim`);
+//         super.move(this.swimSpeed);
+//     }
+// }
 
-class Eagle extends Animal {
-     constructor(name, age, flySpeed) {
-         super(name, age); 
-       this.flySpeed = flySpeed;
+// class Eagle extends Animal {
+//      constructor(name, age, flySpeed) {
+//          super(name, age); 
+//        this.flySpeed = flySpeed;
         
+//     }
+//     fly() {
+//         console.log(`This ${this.name} can fly`);
+//         super.move(this.flySpeed);
+//     }
+
+// }
+
+
+// const tiger = new Tiger("tiger", 2, 30)
+// const fish = new Fish("fish", 3, 20)
+// const eagle = new Eagle("eagle", 4, 35)
+
+// console.log(tiger.name);
+// console.log(tiger.age);
+// console.log(tiger.runSpeed);
+
+// tiger.run();
+// fish.swim();
+
+
+// class Rectangle {
+
+//   constructor(width, height) {
+//      this.width = width;
+//      this.height = height;
+//   }
+
+//  set width(newWidth) {
+//       if(newWidth > 0) {
+//         this._width = newWidth;
+//       } else {
+//         console.error("Width must be a +ve number")
+//       }
+//  }
+
+//  set height(newHeight) {
+//       if(newHeight > 0) {
+//         this._height = newHeight;
+//       } else {
+//         console.error("Height must be a +ve number")
+//       }
+//  }
+
+//  get width() {
+//      return this._width.toFixed(1);
+//  }
+
+//  get height() {
+//     return this._height.toFixed(1);
+//  }
+
+//  get area() {
+//     return this._height * this.width;
+//  }
+
+// }
+
+// const rectangle = new Rectangle();
+
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+// console.log(rectangle.area);
+
+
+class Person{
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
     }
-    fly() {
-        console.log(`This ${this.name} can fly`);
-        super.move(this.flySpeed);
+    
+    set firstName(newFirstName) {
+        if(typeof newFirstName === "string" && newFirstName.length > 0) {
+            this._firstName = newFirstName;
+        } else {
+            console.error("first name must be non empty string");
+        }
     }
+    set lastName(newLastName) {
+        if(typeof newLastName === "string" && newLastName.length > 0) {
+            this._lastName = newLastName;
+        } else {
+            console.error("last name must be non empty string");
+        }
+    }
+
+     set age(newAge) {
+        if(typeof newAge === "number" && newAge >= 0) {
+            this._age = newAge;
+        } else {
+            console.log("age must be non negative number");
+        }
+     }
+
+      get firstName() {
+        return this._firstName;
+      }
+      get lastName() {
+        return this._lastName;
+      }
+      get fullName() {
+        return this._firstName + " " + this._lastName;
+      }
+
+       get age() {
+        return this._age;
+       }
 
 }
 
+const person = new Person("ganesh", "kumar", 22);
 
-const tiger = new Tiger("tiger", 2, 30)
-const fish = new Fish("fish", 3, 20)
-const eagle = new Eagle("eagle", 4, 35)
 
-console.log(tiger.name);
-console.log(tiger.age);
-console.log(tiger.runSpeed);
 
-tiger.run();
-fish.swim();
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.fullName);
+console.log(person.age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
